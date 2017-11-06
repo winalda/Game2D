d3b36275a3a4323a10e0ca81966528519b502473
winalda <alberto@kokonutstudio.com>
2017-11-05 23:12:46 -0600

    Se agregaron componentes
    Se llego al apartado #4

---
+++ Assets/Scrip/CheckGroundad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGroundad : MonoBehaviour {
+
+	PlayerController playerController;
+
+	// Use this for initialization
+	void Start () {
+		playerController = GetComponentInParent<PlayerController> ();
+	}
+
+	void OnCollisionStay2D(Collision2D coll){
+		playerController.groundad = true;
+	}
+
+	void OnCollisionExit2D(Collision2D coll){
+		playerController.groundad = false;
+	}
+}
---
+++ Assets/Scrip/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxVelocity = 5f;
+	public float speed = 20f;
+	private Rigidbody2D rgb2D;
+	private Animator animator;
+	public bool groundad;
+
+	// Use this for initialization
+	void Start () {
+		rgb2D = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat ("speed",Mathf.Abs(rgb2D.velocity.x));
+		animator.SetBool ("Grundead", groundad);
+	}
+
+	void FixedUpdate(){
+
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		rgb2D.AddForce (Vector2.right * speed * horizontal);
+
+		//El valor clamp regresa los limites superirores e inferiorees que puede alcanazar una variable
+		float horizontalVelocity = Mathf.Clamp (rgb2D.velocity.x, -maxVelocity,maxVelocity);
+		rgb2D.velocity =  new Vector2(horizontalVelocity,rgb2D.velocity.y);
+
+		if (horizontal > 0.1) {
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		} else if (horizontal < -0.1) {
+			transform.localScale = new Vector3 (-1f, 1f, 1f);
+		}
+	}
+}
